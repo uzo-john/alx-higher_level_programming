4cd99431b7aaea5b7b310fa0b795ea8ecfe868a2
uzo-john <johnpauluzowuru2018@gmail.com>
2023-03-07 06:24:50 -0800

    task

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,33 +1,26 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
 
 /**
- * insert_node - Inserts a number into a sorted singly-linked list.
- * @head: A pointer the head of the linked list.
- * @number: The number to insert.
- * Return: If the function fails - NULL.
- *         Otherwise - a pointer to the new node.
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-listint_t *insert_node(listint_t **head, int number)
+typedef struct listint_s
 {
-	listint_t *node = *head, *new;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
-	if (node == NULL || node->n >= number)
-	{
-		new->next = node;
-		*head = new;
-		return (new);
-	}
+listint_t *insert_node(listint_t **head, int number);
 
-	while (node && node->next && node->next->n < number)
-		node = node->next;
-
-	new->next = node->next;
-	node->next = new;
-
-	return (new);
-}
+#endif /* LISTS_H */
