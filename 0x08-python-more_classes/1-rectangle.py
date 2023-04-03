b81365bc0eb5d390da9d834e3a8eda878e86df66
uzo-john <johnpauluzowuru2018@gmail.com>
2023-04-03 06:32:56 -0700

    tasks

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-based on a class that defines a Rectangle
-
+defines a Retangle class
 """
 
-
 class Rectangle:
-    """class that define a rectangle"""
+    """to represent a rectangle"""
 
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
 
-        """ method that Initializes the class rectangle"""
+        """ method that Initializes the class rectangle
+        Args:
+            width(int): width of the rectangle
+            height(int): height of the rectangle
+        """
 
         self.height = height
         self.width = width
@@ -26,7 +28,7 @@ class Rectangle:
 
         """setter that define the width of the rectangle"""
 
-        if type(value) is not int:
+        if type(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -35,14 +37,13 @@ class Rectangle:
     @property
     def height(self):
         """method that return the value of the height of the rectangle"""
-        return: 
-		self.__height
+        return self.__height
 
     @height.setter
     def height(self, value):
         """setter that define the height of the rectangle"""
-        if type(value) is not int:
+        if type(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value`
+        self.__height = value
