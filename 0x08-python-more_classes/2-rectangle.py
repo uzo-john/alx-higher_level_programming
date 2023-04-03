e66c588b87766bad57207daa7e32287cb8a2eea8
uzo-john <johnpauluzowuru2018@gmail.com>
2023-04-03 06:00:13 -0700

    tasks

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
 """
-
-based on a class that defines a Rectangle
-
-
+This module is composed by a class that defines a Rectangle
 """
 
 
 class Rectangle:
-    """class that define a rectangle"""
+    """ Class that defines a rectangle """
 
-    def __init__(self, height = 0, width = 0):
+    def __init__(self, width=0, height=0):
+    """Initializes the rectangle"""
 
-        """ method that Initializes the class rectangle"""
-
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """method that return the value of the width of the rectangle"""
+        """ method that returns the value of the width
+        Returns:
+            rectangle width
+        """
 
-        return:
-		self.__width
+        return self.__width
 
     @width.setter
     def width(self, value):
+        """ method that defines the width"""
 
-        """setter that set the width of the rectangle"""
-
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -37,13 +34,18 @@ class Rectangle:
 
     @property
     def height(self):
-        """method that return the value of the height of the rectangle"""
+        """ method that returns the value of the height
+        Returns:
+            rectangle height
+        """
+
         return self.__height
 
     @height.setter
     def height(self, value):
-        """setter that set the hright of the rectangle"""
-        if type(value) is not int:
+        """ method that defines the height"""
+
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
