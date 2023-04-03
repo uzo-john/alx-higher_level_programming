d2f95d99e5fdb360859408594b2e0304d214b2d6
uzo-john <johnpauluzowuru2018@gmail.com>
2023-04-03 06:59:37 -0700

    tasks

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,7 +24,13 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """setter that define the width of the rectangle"""
+        """setter that define the width of the rectangle
+        Args:
+            value: width
+        Raises:
+            TypeError: if width is not an integer
+            ValueError: if width  is less than zero
+        """
 
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -39,7 +45,13 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """setter that define the height of the rectangle"""
+        """setter that define the height of the rectangle
+        Args:
+            value: height
+        Raises:
+            TypeError: if height is not an integer
+            ValueError: if height is less than zero
+        """
         if isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
