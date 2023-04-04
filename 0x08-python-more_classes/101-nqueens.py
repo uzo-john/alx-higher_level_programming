33c033183ef158188838124b0afcb816a6a3fe22
uzo-john <johnpauluzowuru2018@gmail.com>
2023-04-04 07:47:35 -0700

    advance task

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """
 the N queens puzzle is the challenge of placing N non-attacking queens
-this program will solves the N queens problem.
+this program will solves the N quee
 """
-
-
 from sys import argv
 
 if __name__ == "__main__":
@@ -15,24 +13,24 @@ if __name__ == "__main__":
     if argv[1].isdigit() is False:
         print("N must be a number")
         exit(1)
-    n = int(argv[1])
-    if n < 4:
+    N = int(argv[1])
+    if N < 4:
         print("N must be at least 4")
         exit(1)
 
     # initialize the answer list
-    for i in range(n):
+    for i in range(N):
         a.append([i, None])
 
     def already_exists(y):
-        """method that check if the queen does not already exist in that y value"""
-        for x in range(n):
+        """check whether the queen exist already in y value"""
+        for x in range(N):
             if y == a[x][1]:
                 return True
         return False
 
     def reject(x, y):
-        """method that determines whether or not to reject the solution"""
+        """To check whether the solution is rejected or not"""
         if (already_exists(y)):
             return False
         i = 0
@@ -43,17 +41,17 @@ if __name__ == "__main__":
         return True
 
     def clear_a(x):
-        """clears the answers from the point of failure on"""
+        """answers from the point of failure on"""
         for i in range(x, n):
             a[i][1] = None
 
     def nqueens(x):
         """recursive backtracking function to find the solution"""
-        for y in range(n):
+        for y in range(N):
             clear_a(x)
             if reject(x, y):
                 a[x][1] = y
-                if (x == n - 1):  # accepts the solution
+                if (x == N - 1):  # accepts the solution
                     print(a)
                 else:
                     nqueens(x + 1)  # moves on to next x value to continue
