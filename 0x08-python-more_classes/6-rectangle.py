c7793abdfbbbf4c6cf8c9d602803900d54a9a9b0
uzo-john <johnpauluzowuru2018@gmail.com>
2023-04-04 04:26:22 -0700

    task

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,6 +5,8 @@
 class Rectangle:
     """Represent a rectangle."""
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
         Args:
@@ -13,6 +15,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -72,5 +75,7 @@ class Rectangle:
 
     def __del__(self):
         """method to print a message for every delection of a rectangle"""
+
         Rectangle.number_of_instances -= 1
+
         print("Bye rectangle...")
