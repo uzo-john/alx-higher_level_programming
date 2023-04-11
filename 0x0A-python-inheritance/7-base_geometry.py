db5127d326a0d79b1d24b07e20460ed1c048638a
uzo-john <johnpauluzowuru2018@gmail.com>
2023-04-11 06:30:43 -0700

    tasks

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """ Class that defines the attributes of Geometric Shapes """
+
+    def area(self):
+        """method that defines the area of the geometric"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Method that recieves the value property
+        Árgs:
+            name: name of the object
+            value: value of the property
+        """
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
