82611692491b44177c84f9f711b9e4c170552f67
uzo-john <johnpauluzowuru2018@gmail.com>
2023-04-11 04:35:52 -0700

    tasks

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-This method  contains the function is_same_class
+This method contains the function is_same_class
 """
 
 
 def is_same_class(obj, a_class):
     """return true if obj is the exact class a_class, otherwise false
-    
+
     Args:
 	obj: object
 	a_class: class type
