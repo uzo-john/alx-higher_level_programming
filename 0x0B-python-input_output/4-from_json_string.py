72684aaca61ad84ee4966ed7eaa7cf02e3ef0a72
uzo-john <johnpauluzowuru2018@gmail.com>
2023-04-12 05:39:36 -0700

    tasks

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" function that returns an object represented by a JSON string"""
+"""
+contains the json str function
+"""
 
 import json
 
 
 def from_json_string(my_str):
-    """object represented by a JSON string"""
+
     return json.loads(my_str)
