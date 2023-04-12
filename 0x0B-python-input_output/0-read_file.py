f03f359644b7c0dbc158fffbcdf4072140a5015d
uzo-john <johnpauluzowuru2018@gmail.com>
2023-04-12 04:29:29 -0700

    tasks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Method that defines a text file reading function"""
+"""function that read a file"""
 
 
 def read_file(filename=""):
+    """reads a text file(UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
