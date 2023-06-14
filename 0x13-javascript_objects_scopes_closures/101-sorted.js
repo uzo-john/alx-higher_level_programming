94f5a8be04a3877765624f259f8cd502beac277a
uzo-john <johnpauluzowuru2018@gmail.com>
2023-06-14 13:42:01 -0700

    advance task

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const newList = list.map(function (num, index) {
-  return num * index;
+const dict = require('./101-data').dict;
+const newDiction = {};
+
+Object.keys(dict).map(function (key, index) {
+  if (newDiction[dict[key]] === undefined) {
+    newDiction[dict[key]] = [];
+  }
+  newDiction[dict[key]].push(key);
 });
 
-console.log(list);
-console.log(newList);
+console.log(newDiction);
