55721b5ed71a020a675fddf02845ba15558252ba
uzo-john <johnpauluzowuru2018@gmail.com>
2023-06-19 13:06:43 -0700

    tasks

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-Prints the State object with the name passed as argument from the db
+Prints all City objects from the database hbtn_0e_14_usa
 """
 import sys
 from model_state import Base, State
+from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -15,5 +16,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
-    print("Not found" if not state else state.id)
+    st_cty = session.query(State, City).filter(State.id == City.state_id).all()
+
+    for state, city in st_cty:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
