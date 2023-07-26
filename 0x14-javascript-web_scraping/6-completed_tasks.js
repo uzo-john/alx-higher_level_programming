7d3f5312bc272a79f65765b3713792d135a9b2f1
uzo-john <johnpauluzowuru2018@gmail.com>
2023-07-26 02:56:44 -0700

    tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-/*  computes the number of tasks completed by user id. */
-
 const request = require('request');
-const url = process.argv[2];
 
-request(url, (err, response, body) => {
-  if (err) console.log(err);
-  else {
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
     const resp = {};
     const json = JSON.parse(body);
     for (let i = 0; i < json.length; i++) {
